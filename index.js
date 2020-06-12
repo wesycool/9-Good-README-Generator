be53ee83b0b0ac2ff0a2f14ef2f9177753c80a58
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-12 09:11:59 -0400

    Added writeToFile function

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
 const questions = [
+
 
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+        if (err) return console.log(err);
+        console.log("Success!");
+    })
 }
 
 function init() {
