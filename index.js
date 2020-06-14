ba10ad4e4881d835c0dc8d5ecd0f1b97b28971d7
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-14 15:35:59 -0400

    Testing to generate README.md file dynamically

--- index.js
+++ index.js
@@ -1,20 +1,84 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const axios = require('axios')
 
 const questions = [
-
-
+    {
+        type:'input',
+        message: 'What is your username?',
+        name: 'user',
+    },
+    {
+        type:'input',
+        message: 'What is your project name?',
+        name: 'title' ,
+    },
+    {
+        type: 'input',
+        message: 'What is this project about?',
+        name: 'description',
+    },
+    {
+        type:'list',
+        message: 'What kind of file is this?',
+        name: 'filename',
+        choices: ['README.md']
+    }
 ];
 
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+
+
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, function(err) {
         if (err) return console.log(err);
-        console.log("Success!");
+        console.log(`Success! ${filename} file was created.`);
     })
 }
 
-function init() {
 
+async function init() {
+    const getQuestion = await inquirer.prompt(questions)
+    const fetchGitHub = await axios.get(`https://api.github.com/users/${getQuestion.user}`)
+
+
+const content = 
+`# ${getQuestion.title}
+${getQuestion.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
+
+This project was created by: ${fetchGitHub.data.name}`
+
+    writeToFile(getQuestion.filename,content)
 }
 
 init();
+
+
+// * Title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
